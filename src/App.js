63a91ffc196b7aa0cd8da0ebc8564f877b07cbda
Adi Rozenberg <104524355+adirozenberg@users.noreply.github.com>
2023-01-02 19:37:06 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MainGame from "./Components/MainGame";
+
+const App = () => {
+  return (
+    <div className="App">
+      <MainGame />
+    </div>
+  );
+};
+
+export default App;
